7c08f43cafa5328401311dc33b9685f9fe040b4c
Luiza Campos <luiza.almcampos@gmail.com>
2021-04-23 17:49:30 -0300

    type episode arrumado

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -15,7 +15,7 @@ type Episode = {
     title: string;
     thumbnail: string;
     members: string;
-    duration: string;
+    duration: number;
     durationAsString: string;
     url: string;
     publishedAt: string;
